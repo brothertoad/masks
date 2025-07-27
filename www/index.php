5bba5b40a406a89d24cb18b42c4b9ddae94d03ae
Stacey Brogden <toad@brothertoad.net>
2025-07-26 19:03:32 -0700

    Add index.php and site.css to www.

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<link rel="stylesheet" href="site.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">	<title>Masks</title>
+</head>
+<body>
+	<div class="main-content">
+		<h3>Legacy links</h3>
+		<ul>
+			<li><a href="mask.html">Original masks</a></li>
+			<li><a href="centered-mask.html">Centered mask.svg</a></li>
+			<li><a href="mask.svg">Mask.svg</a></li>
+		</ul>
+	</div>
+</body>
+</html>
