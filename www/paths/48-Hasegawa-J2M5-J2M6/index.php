c55c65cd979ffde9bd6fb2c19ba94de11d29c15d
Stacey Brogden <toad@brothertoad.net>
2025-07-28 18:20:23 -0700

    Start implementing a sample path page.

---
+++ www/paths/48-Hasegawa-J2M5-J2M6/index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/site.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<title>48-Hasegawa-J2M5-J2M6</title>
+</head>
+<body>
+	<div class="main-content">
+		<div>
+			<h3>48-Hasegawa-J2M5-J2M6 images</h3>
+			<ul>
+				<?php
+					$images = scandir(".");
+					foreach ($images as $image) {
+						if (endsWith($image, ".png") || endsWith($image, ".jpg")) {
+							echo("<li><a href=\"$image/\">$image</a></li>");
+						}
+					}
+					function endsWith($string, $endString) {
+					    return substr($string, -strlen($endString) ) === $endString;
+					}
+				?>
+			</ul>
+		</div>
+		<div>
+			<a href="/">Home</a>
+		</div>
+	</div>
+</body>
+</html>
