61ecae4f58d39767845d48b1254457b145c58058
Stacey Brogden <toad@brothertoad.net>
2025-07-31 15:36:23 -0700

    More updates to path page styling.

--- www/paths/48-Hasegawa-J2M5-J2M6/s1-800.php
+++ www/paths/48-Hasegawa-J2M5-J2M6/s1-800.php
@@ -14,11 +14,22 @@
 				<path d="M 100 100 L 200 100 L 200 200 L 100 200 Z" />
 			</svg>
 		</div>
-		<div class="has-rows cell">
+		<div class="column cell">
 			<div class="row">
+				<div class="row-item">
+					<span>Filled when clicked</span>
+				</div>
+				<div class="row-item">
+					<button type="button">Clear</button>
+				</div>
 			</div>
 			<div class="row">
-				<a href="./" class="xxxcentered-child">Project page</a>
+				<div class="row-item">
+					<a href="./">Project page</a>
+				</div>
+				<div class="row-item">
+					<a href="/">Main page</a>
+				</div>
 			</div>
 		</div>
 	</div>
