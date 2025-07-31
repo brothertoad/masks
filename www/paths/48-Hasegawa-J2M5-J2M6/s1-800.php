5b5bb380abeb9b7ef6a1365a04948d882ac65fd9
Stacey Brogden <toad@brothertoad.net>
2025-07-30 19:31:43 -0700

    Final version of s1-800 files.

--- www/paths/48-Hasegawa-J2M5-J2M6/s1-800.php
+++ www/paths/48-Hasegawa-J2M5-J2M6/s1-800.php
@@ -11,7 +11,7 @@
 	<div class="main-content">
 		<div class="image-div">
 			<svg>
-				<path d="M 100 100 L 200 100 L 200 200 L 100 200 Z" fill="none" stroke="red" />
+				<path d="M 100 100 L 200 100 L 200 200 L 100 200 Z" />
 			</svg>
 		</div>
 		<div class="centered-parent">
