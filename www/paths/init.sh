27361a83855a3311b5e66c5cac0de093caab7b4e
Stacey Brogden <toad@brothertoad.net>
2025-08-01 11:32:12 -0700

    Create php and css files in init.sh.

--- www/paths/init.sh
+++ www/paths/init.sh
@@ -16,6 +16,22 @@ fi
 
 for imgfile in "$@"; do
 	imgname=`echo $imgfile | rev | cut -f2- -d. | rev`
+	phpname="$imgname.php"
+	cssname="$imgname.css"
 	read imgw imgh < <(identify -format "%w %h" $imgfile)
 	echo imgfile is $imgfile, imgname is $imgname, size is $imgw by $imgh
+	if [ -f "$phpname" ]; then
+		echo $phpname exists, not overwriting
+	else
+		sed s/PROJECT/$project/g < ../../../template/path.php | \
+			sed s/IMGNAME/$imgname/g | sed s/IMGFILE/$imgfile/g | \
+			sed s/IMGW/$imgw/g | sed s/IMGH/$imgh/g > "$phpname"
+	fi
+	if [ -f "$cssname" ]; then
+		echo $cssname exists, not overwriting
+	else
+		sed s/PROJECT/$project/g < ../../../template/path.css | \
+			sed s/IMGNAME/$imgname/g | sed s/IMGFILE/$imgfile/g | \
+			sed s/IMGW/$imgw/g | sed s/IMGH/$imgh/g > "$cssname"
+	fi
 done
