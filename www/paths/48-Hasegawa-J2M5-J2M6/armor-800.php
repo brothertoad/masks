b8e91ac988f17e0ec07b4181548488190c6e55ad
Stacey Brogden <toad@brothertoad.net>
2025-08-03 10:37:42 -0700

    Create armor path on J2M6.

--- www/paths/48-Hasegawa-J2M5-J2M6/armor-800.php
+++ www/paths/48-Hasegawa-J2M5-J2M6/armor-800.php
@@ -11,7 +11,7 @@
 	<div class="main-content">
 		<div class="image-div cell" onclick="addCoords(event);">
 			<svg>
-				<path d="M 100 100 L 200 100 L 200 200 L 100 200 Z" />
+				<path d="M 148 40 L 486 194 L 488 544 L 148 694 Z" />
 			</svg>
 		</div>
 		<div class="column cell">
