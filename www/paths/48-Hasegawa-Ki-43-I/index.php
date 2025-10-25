80fdaf8a2f160860180d29781e13b32e0df99940
Stacey Brogden <toad@brothertoad.net>
2025-10-25 16:06:29 -0700

    Initial commit of Hasegawa Ki43-I.

---
+++ www/paths/48-Hasegawa-Ki-43-I/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/site.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<title>48-Hasegawa-Ki-43-I</title>
+</head>
+<body>
+	<div class="main-content">
+		<div>
+			<h3>48-Hasegawa-Ki-43-I images</h3>
+			<ul>
+				<?php
+					$images = scandir(".");
+					foreach ($images as $image) {
+						// We have PHP 7, so str_ends_with is not available.
+						$ext = substr($image, -4);
+						if ($ext == ".png" || $ext == ".jpg") {
+							$base = substr($image, 0, strlen($image) - 4);
+							$target = $base . ".php";
+							if (file_exists($target)) {
+								echo("<li><a href=\"$target\">$base</a></li>");
+							}
+						}
+					}
+				?>
+			</ul>
+		</div>
+			<?php
+				$svg = "48-Hasegawa-Ki-43-I.svg";
+				if (file_exists($svg)) {
+					echo("<div>");
+					echo("<img src=\"$svg\" alt=\"full SVG\">");
+					echo ("</div>");
+					echo("<div>");
+					echo("<a href=\"$svg\">Project SVG</a>");
+					echo ("</div>");
+				}
+			?>
+		<div>
+			<a href="/">Home</a>
+		</div>
+	</div>
+</body>
+</html>
