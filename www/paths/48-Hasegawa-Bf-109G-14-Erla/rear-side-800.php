edaad24bf6da589d718bee73df1365b7aa9ced36
Stacey Brogden <toad@brothertoad.net>
2025-12-05 20:37:18 -0800

    First cut at Hasegawa G-14 Erla.

---
+++ www/paths/48-Hasegawa-Bf-109G-14-Erla/rear-side-800.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/site.css">
+	<link rel="stylesheet" href="rear-side-800.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<title>rear-side-800</title>
+</head>
+<body>
+	<div class="main-content">
+		<div class="image-div cell" onclick="addCoords(event);">
+			<svg>
+				<path d="M 96 56 L 510 62 L 510 684 L 96 684 Z" />
+				<path d="M 704 64 C 1028 60, 1346 120, 1672 234 L 1420 512 Q 1310 652, 1110 684 L 704 684 Z" />
+			</svg>
+		</div>
+		<div class="column cell">
+			<div class="row">
+				<div class="row-item">
+					<input id="coords" value="" size="32" readonly />
+				</div>
+				<div class="row-item">
+					<button type="button" onclick="clearCoords();">Clear</button>
+				</div>
+			</div>
+			<div class="row">
+				<div class="row-item">
+					<a href="./">Project page</a>
+				</div>
+				<div class="row-item">
+					<a href="/">Main page</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		function clearCoords() {
+			document.getElementById("coords").value = "";
+		}
+		function addCoords(event) {
+			var coords = document.getElementById("coords");
+			coords.value = coords.value + " " + event.offsetX + "," + event.offsetY;
+		}
+	</script>
+</body>
+</html>
---
+++ www/paths/48-Hasegawa-Bf-109G-14-Erla/windscreen-side-800.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/site.css">
+	<link rel="stylesheet" href="windscreen-side-800.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<title>windscreen-side-800</title>
+</head>
+<body>
+	<div class="main-content">
+		<div class="image-div cell" onclick="addCoords(event);">
+			<svg>
+				<path d="M 404 106 L 736 372 Q 796 418, 796 520 L 786 634 L 66 622 Z" />
+			</svg>
+		</div>
+		<div class="column cell">
+			<div class="row">
+				<div class="row-item">
+					<input id="coords" value="" size="32" readonly />
+				</div>
+				<div class="row-item">
+					<button type="button" onclick="clearCoords();">Clear</button>
+				</div>
+			</div>
+			<div class="row">
+				<div class="row-item">
+					<a href="./">Project page</a>
+				</div>
+				<div class="row-item">
+					<a href="/">Main page</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		function clearCoords() {
+			document.getElementById("coords").value = "";
+		}
+		function addCoords(event) {
+			var coords = document.getElementById("coords");
+			coords.value = coords.value + " " + event.offsetX + "," + event.offsetY;
+		}
+	</script>
+</body>
+</html>
