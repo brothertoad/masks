804d2c5a9e231611d336db0debfd11a187b13203
Stacey Brogden <toad@brothertoad.net>
2026-01-02 19:00:51 -0800

    First cut at Hasegawa Razorback.

---
+++ www/paths/48-Hasegawa-P-47D-Razorback/rear2-800.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/site.css">
+	<link rel="stylesheet" href="rear2-800.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<title>rear2-800</title>
+</head>
+<body>
+	<div class="main-content">
+		<div class="image-div cell" onclick="addCoords(event);">
+			<svg>
+				<path d="M 116 128 L 598 128 L 624 692 L 142 692 Z" />
+			</svg>
+		</div>
+		<div class="column cell">
+			<div class="row">
+				<div class="row-item">
+					<input id="coords" value="" size="32" readonly />
+				</div>
+				<div class="row-item">
+					<button type="button" onclick="clearCoords();">Clear</button>
+				</div>
+			</div>
+			<div class="row">
+				<div class="row-item">
+					<a href="./">Project page</a>
+				</div>
+				<div class="row-item">
+					<a href="/">Main page</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		function clearCoords() {
+			document.getElementById("coords").value = "";
+		}
+		function addCoords(event) {
+			var coords = document.getElementById("coords");
+			coords.value = coords.value + " " + event.offsetX + "," + event.offsetY;
+		}
+	</script>
+</body>
+</html>
---
+++ www/paths/48-Hasegawa-P-47D-Razorback/wside-800.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/site.css">
+	<link rel="stylesheet" href="wside-800.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<title>wside-800</title>
+</head>
+<body>
+	<div class="main-content">
+		<div class="image-div cell" onclick="addCoords(event);">
+			<svg>
+				<path d="M 60 154 L 416 44   Q 544 214, 772 316   L 592 720   Q 268 472, 60 154" />
+			</svg>
+		</div>
+		<div class="column cell">
+			<div class="row">
+				<div class="row-item">
+					<input id="coords" value="" size="32" readonly />
+				</div>
+				<div class="row-item">
+					<button type="button" onclick="clearCoords();">Clear</button>
+				</div>
+			</div>
+			<div class="row">
+				<div class="row-item">
+					<a href="./">Project page</a>
+				</div>
+				<div class="row-item">
+					<a href="/">Main page</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		function clearCoords() {
+			document.getElementById("coords").value = "";
+		}
+		function addCoords(event) {
+			var coords = document.getElementById("coords");
+			coords.value = coords.value + " " + event.offsetX + "," + event.offsetY;
+		}
+	</script>
+</body>
+</html>
